03d63d7d5baa6962fc18cbff4f5dc6083ac96974
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-10-03 14:42:02 -0700

    MFT site version 1.0.0; generated nav bar skeleton.

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,33 @@ class App extends Component {
     const { devFirstName, devLastName } = this.state;
     return (
       <div>
-        <div className="title-head">
-          <h1>{`This site is under construction by ${devFirstName} ${devLastName}`}</h1>
-        </div>
+        <header className="site-header group">
+          <h1>MFT 4 ME</h1>
+          <nav className="main-nav">
+            <ul>
+              <li>
+                <a href="none">Home</a>
+              </li>
+              <li>
+                <a href="none">Book</a>
+              </li>
+              <li>
+                <a href="none">About</a>
+              </li>
+              <li>
+                <a href="none">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <p>{`This site is under construction by ${devFirstName} ${devLastName}`}</p>
+        </main>
+        <footer>
+          <p>
+            MFT 4 ME &copy; 2018
+          </p>
+        </footer>
       </div>
     );
   }
